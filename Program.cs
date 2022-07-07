4a6dab7d76f48556b8fe9d9680509c47adbfde90
Konstantin-AKV <abdulaev-kostya@mail.ru>
2022-07-07 23:32:44 +0300

    Задача 19

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 19: Палиндром или нет?
+Console.WriteLine("Введите пятизначное число: ");
+int N = int.Parse(Console.ReadLine());
+
+int a1 = N / 10000;
+int a2 = N % 10;
+int a3 = N / 1000 % 10;
+int a4 = N % 100 / 10;
+
+if (a1 == a2 && a3 == a4)
+{
+    Console.WriteLine("палиндром");
+}
+else Console.WriteLine("не палиндром");
