f6b421285a6043ea6b70250b5283273d6dc03ca7
Konstantin-AKV <abdulaev-kostya@mail.ru>
2022-07-07 23:42:53 +0300

    Задача 21

--- Program.cs
+++ Program.cs
@@ -1,14 +1,38 @@
 ﻿// Задача 19: Палиндром или нет?
-Console.WriteLine("Введите пятизначное число: ");
+/* Console.WriteLine("Введите пятизначное число: ");
 int N = int.Parse(Console.ReadLine());
-
 int a1 = N / 10000;
 int a2 = N % 10;
 int a3 = N / 1000 % 10;
 int a4 = N % 100 / 10;
-
 if (a1 == a2 && a3 == a4)
 {
-    Console.WriteLine("палиндром");
+    Console.WriteLine("Палиндром");
 }
-else Console.WriteLine("не палиндром");
+else Console.WriteLine("а это уже не палиндром");
+ */
+
+// Задача 21: Координаты в 3D пространстве.
+
+Console.WriteLine("Введите x1: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите z1: ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите x2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2));
+Console.WriteLine(Math.Round(result,2)); 
+
+
